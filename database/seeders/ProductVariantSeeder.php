13e7b97fbaebd06c05e82eb0a33b37353d45ba50
Nasz11 <g22-0708-392@gma.uphsl.edu.ph>
2026-04-05 13:08:40 +0800

    Add ProductVariant seeder

---
+++ database/seeders/ProductVariantSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\ProductVariant;
+
+class ProductVariantSeeder extends Seeder
+{
+    public function run(): void
+    {
+        ProductVariant::truncate();
+
+        $variants = [
+            ['id'=>2,  'cheese_product'=>'Burrata',           'variant_name'=>'50g',          'weight_grams'=>50],
+            ['id'=>3,  'cheese_product'=>'Burrata',           'variant_name'=>'125g',         'weight_grams'=>125],
+            ['id'=>4,  'cheese_product'=>'Burrata',           'variant_name'=>'150g',         'weight_grams'=>150],
+            ['id'=>5,  'cheese_product'=>'Burrata',           'variant_name'=>'250g Solo',    'weight_grams'=>250],
+            ['id'=>6,  'cheese_product'=>'Burrata',           'variant_name'=>'250g Duo',     'weight_grams'=>250],
+            ['id'=>7,  'cheese_product'=>'Burrata',           'variant_name'=>'Cherry',       'weight_grams'=>125],
+            ['id'=>8,  'cheese_product'=>'Burrata',           'variant_name'=>'Jamon',        'weight_grams'=>125],
+            ['id'=>9,  'cheese_product'=>'Burrata',           'variant_name'=>'Truffle Combi','weight_grams'=>125],
+            ['id'=>10, 'cheese_product'=>'Burrata',           'variant_name'=>'200g',         'weight_grams'=>200],
+            ['id'=>11, 'cheese_product'=>'Burrata',           'variant_name'=>'200g Truffle', 'weight_grams'=>200],
+            ['id'=>12, 'cheese_product'=>'Burrata',           'variant_name'=>'2/250g',       'weight_grams'=>500],
+            ['id'=>14, 'cheese_product'=>'Stracciatella',     'variant_name'=>'Reg Tub',      'weight_grams'=>500],
+            ['id'=>15, 'cheese_product'=>'Stracciatella',     'variant_name'=>'250g',         'weight_grams'=>250],
+            ['id'=>16, 'cheese_product'=>'Stracciatella',     'variant_name'=>'334g',         'weight_grams'=>334],
+            ['id'=>17, 'cheese_product'=>'Stracciatella',     'variant_name'=>'668g',         'weight_grams'=>668],
+            ['id'=>18, 'cheese_product'=>'Stracciatella',     'variant_name'=>'Combi',        'weight_grams'=>500],
+            ['id'=>19, 'cheese_product'=>'Mozzarella Log',    'variant_name'=>'1kg',          'weight_grams'=>1000],
+            ['id'=>20, 'cheese_product'=>'Provola',           'variant_name'=>'1kg',          'weight_grams'=>1000],
+            ['id'=>21, 'cheese_product'=>'Cherry Mozzarella', 'variant_name'=>'250g',         'weight_grams'=>250],
+            ['id'=>22, 'cheese_product'=>'Cherry Mozzarella', 'variant_name'=>'1kg',          'weight_grams'=>1000],
+        ];
+
+        foreach ($variants as $v) {
+            ProductVariant::create($v);
+        }
+    }
+}
